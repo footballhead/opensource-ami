51ed6738cdf0815153e6472e8ecad0d2c471e456
Robin Eklind <rnd0x00@gmail.com>
2020-02-14 00:25:37 +0100

    tools: gofmt

--- tools/tools.go
+++ tools/tools.go
@@ -3,12 +3,12 @@
 package tools
 
 import (
-	_ "github.com/sanctuary/mpq"
 	_ "github.com/mewrnd/blizzconv/cmd/mpqfix"
 	_ "github.com/sanctuary/formats/cmd/cel_dump"
-	_ "github.com/sanctuary/formats/cmd/min_dump"
-	_ "github.com/sanctuary/formats/cmd/til_dump"
 	_ "github.com/sanctuary/formats/cmd/dun_dump"
 	_ "github.com/sanctuary/formats/cmd/dun_merge"
+	_ "github.com/sanctuary/formats/cmd/min_dump"
+	_ "github.com/sanctuary/formats/cmd/til_dump"
+	_ "github.com/sanctuary/mpq"
 	_ "github.com/sanctuary/opensource-ami/cmd/miniset_dump"
 )
